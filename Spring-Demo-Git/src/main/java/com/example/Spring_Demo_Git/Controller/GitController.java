4b7f0efd4930c81c4cf7b89a1e5a8501a7c18b7d
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2025-06-02 12:52:59 +0530

    my first commit

--- Spring-Demo-Git/src/main/java/com/example/Spring_Demo_Git/Controller/GitController.java
+++ Spring-Demo-Git/src/main/java/com/example/Spring_Demo_Git/Controller/GitController.java
@@ -1,10 +1,11 @@
 package com.example.Spring_Demo_Git.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitController {
-
+@GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
