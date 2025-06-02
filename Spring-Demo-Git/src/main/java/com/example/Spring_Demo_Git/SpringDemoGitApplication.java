da48096c4e4a6e6d5fccfe938fdd61991ad00de0
shaileshsapkalgit <164751771+shaileshsapkalgit@users.noreply.github.com>
2025-06-02 12:47:19 +0530

    Add files via upload

---
+++ Spring-Demo-Git/src/main/java/com/example/Spring_Demo_Git/SpringDemoGitApplication.java
@@ -0,0 +1,14 @@
+package com.example.Spring_Demo_Git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoGitApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ Spring-Demo-Git/src/test/java/com/example/Spring_Demo_Git/SpringDemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Demo_Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
